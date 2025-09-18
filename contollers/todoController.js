2e11dc55823e6bf3ddca9ef6cb51f611074494a1
koushik2002vas5 <koushiksai.vas5@gmail.com>
2025-09-17 22:37:17 -0400

    successfully built add and delete logic to the todoList

--- contollers/todoController.js
+++ contollers/todoController.js
@@ -1,11 +1,40 @@
+// var bodyParser = require('body-parser');
+// var data = [{item:'get milk'},{item:'walk dog'},{item:'drinking water'}];
+// var urlencodedParser = bodyParser.urlencoded({extended: false});
+// module.exports = function(app){
+// app.get('/todo',function(req,res){
+//     res.render('todoView',{todos: data});
+// });
+// app.post('/todo',urlencodedParser,function(req,res){
+//     data.push(req.body);
+//     res.json(data);
+// });
+// app.delete('/todo/:item',function(req,res){
+//     data = data.filter(function(todo){
+//         return todo.item.replace(/ /g,'-') !== req.params.item;
+//     });
+//     res.json(data);
+// });
+// };
+
+var bodyParser = require('body-parser');
+var data = [{item:'get milk'},{item:'walk dog'},{item:'drinking water'}];
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+
 module.exports = function(app){
-app.get('/todo',function(req,res){
-    res.render('todoView');
-});
-app.post('/todo',function(req,res){
-
-});
-app.delete('/todo',function(req,res){
-
-});
+    app.get('/todo',function(req,res){
+        res.render('todoView',{todos: data});
+    });
+    
+    app.post('/todo',urlencodedParser,function(req,res){
+        data.push(req.body);
+        res.json(data);
+    });
+    
+    app.delete('/todo/:item',function(req,res){
+        data = data.filter(function(todo){
+            return todo.item.replace(/ /g,'-') !== req.params.item;
+        });
+        res.json(data);
+    });
 };
--- public/assets/todo-list.js
+++ public/assets/todo-list.js
@@ -1,34 +1,76 @@
+// $(document).ready(function(){
+
+//   $('form').on('submit', function(){
+
+//       var item = $('form input');
+//       var todo = {item: item.val()};
+
+//       $.ajax({
+//         type: 'POST',
+//         url: '/todo',
+//         data: todo,
+//         success: function(data){
+//           //do something with the data via front-end framework
+//           location.reload();
+//         }
+//       });
+
+//       return false;
+
+//   });
+
+//   $('li').on('click', function(){
+//       var item = $(this).text().replace(/ /g, "-");
+//       $.ajax({
+//         type: 'DELETE',
+//         url: '/todo/' + item,
+//         success: function(data){
+//           //do something with the data via front-end framework
+//           location.reload();
+//         }
+//       });
+//   });
+
+// });
+
 $(document).ready(function(){
+    // Handle form submission for adding new items
+    $('form').on('submit', function(e){
+        e.preventDefault(); // Better than return false
+        
+        var item = $('form input');
+        var todo = {item: item.val()};
 
-  $('form').on('submit', function(){
+        $.ajax({
+            type: 'POST',
+            url: '/todo',
+            data: todo,
+            contentType: 'application/x-www-form-urlencoded',
+            success: function(data){
+                item.val(''); // Clear input field
+                location.reload();
+            },
+            error: function(xhr, status, error) {
+                console.log('Error adding item:', error);
+                alert('Error adding item. Please try again.');
+            }
+        });
+    });
 
-      var item = $('form input');
-      var todo = {item: item.val()};
-
-      $.ajax({
-        type: 'POST',
-        url: '/todo',
-        data: todo,
-        success: function(data){
-          //do something with the data via front-end framework
-          location.reload();
-        }
-      });
-
-      return false;
-
-  });
-
-  $('li').on('click', function(){
-      var item = $(this).text().replace(/ /g, "-");
-      $.ajax({
-        type: 'DELETE',
-        url: '/todo/' + item,
-        success: function(data){
-          //do something with the data via front-end framework
-          location.reload();
-        }
-      });
-  });
-
+    // Handle click events for deleting items (using event delegation)
+    $(document).on('click', 'li', function(){
+        var item = $(this).text().replace(/ /g, "-");
+        
+        $.ajax({
+            type: 'DELETE',
+            url: '/todo/' + item,
+            success: function(data){
+                location.reload();
+            },
+            error: function(xhr, status, error) {
+                console.log('Error deleting item:', error);
+                alert('Error deleting item. Please try again.');
+            }
+        });
+    });
 });
